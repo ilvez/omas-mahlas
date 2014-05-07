a031c57a71f73bf9fa2eea9dfe6dca6e2c262971
Taavi Ilves <taavi.ilves@gmail.com>
2014-05-07 21:06:18 +0300

    test

--- data-compiler/compiler.py
+++ data-compiler/compiler.py
@@ -1,1 +1,11 @@
-import csv
+import csv, json
+
+def extract_csv(file):
+	data = []
+	with open(file, 'rb') as csvfile:
+		csvdata = csv.reader(csvfile, delimiter=',', quotechar='"')
+		print(csvdata)
+		for row in csvdata:
+			print (row)
+
+extract_csv("../test-data/omas-mullis-input.csv")
