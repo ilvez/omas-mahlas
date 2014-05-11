259940c388d1b6e215a9e61beed351a08decf5c0
Taavi Ilves <taavi.ilves@gmail.com>
2014-05-11 14:42:33 +0300

    macosx töötav lowercase

--- scripts/lowercase.sh
+++ scripts/lowercase.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-find -name "* *" -exec rename 's/ /_/g' {} \;
+#find -name "* *" -exec rename 's/ /_/g' {} \;
 
-find screenshots -exec rename 'y/A-Z/a-z/' {} \;
-find sounds -exec rename 'y/A-Z/a-z/' {} \;
+# Linux
+#find screenshots -exec rename 'y/A-Z/a-z/' {} \;
+#find sounds -exec rename 'y/A-Z/a-z/' {} \;
+
+# Macos case insensitive rename workaround
+#find . -name '*.*' -exec sh -c '
+#  a=$(echo {} | tr '[:upper:]' '[:lower:]');
+#  [ "$a" != "{}" ] && mv "{}" "$a" ' \;
