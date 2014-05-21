8174aa43cf9aa53fb5a8e844425427397a5589bc
Taavi Ilves <taavi.ilves@gmail.com>
2014-05-22 00:38:59 +0300

    muudatused mapvideos, päris süngis streetiga pole aga enamvähem

--- presentation/mapvideo/mapvideo.php
+++ presentation/mapvideo/mapvideo.php
@@ -12,18 +12,13 @@
 <script>
 var allElements = [];
 var currentElem = 0;
-var currentVideo = "";
-var currentBegin = 0;
-var currentEnd = 0;
 var currentVideoLength = 0;
 var currentSpeed = -1;
 
 function animate_lamp_icon(lamp){
-
     $("#lamp_icon").fadeOut(1);
     $("#lamp_icon").attr("src", "/data/images/lamp_icons/"+lamp+".svg");
     $("#lamp_icon").delay(2).fadeIn(50).delay(1000).fadeOut(300);;
-
 }
 
 function display_status(name){
@@ -31,58 +26,41 @@ function display_status(name){
 }
 
 function playNewVideo(elem) {
-    currentVideo = elem.mapvideo;
-    $("#mapvidjo-src").attr("src", currentVideo);
+    $("#mapvidjo-src").attr("src", elem.mapvideo);
     var player = $("#mapvidjo")[0];
+    player.playbackRate = 0.0001;
     player.load();
     player.play();
-    console.log("Playing: " + currentVideo);
-}
-
-function getSpeed() {
-    var speed = -1;
-    if (currentEnd != 0 && currentBegin != 0 && currentVideoLength != 0) {
-        var ownTimeDuration = currentEnd - currentBegin;
-        var ownTimeVideoSpeed = ownTimeDuration / currentVideoLength;
-        var step = calculateSpeedup(allElements, timerMap);
-        speed = step * ownTimeVideoSpeed;
-        console.log("currentVideoLength: " + currentVideoLength);
-        console.log("ownTimeDuration: " + ownTimeDuration);
-        console.log("ownTimeVideoSpeed: " + ownTimeVideoSpeed);
-        console.log("step: " + step);
-        console.log("speed: " + speed);
-    }
-    // TODO ROUND
-    return speed;
+    console.log("Playing: " + elem.mapvideo);
 }
 
 function setSpeed(speed) {
-    $("#mapvidjo")[0].playbackRate = speed;
-    currentSpeed = speed;
-    console.log("Setting new speed: " + speed);
+    if (isNaN(speed) == false) {
+        $("#mapvidjo")[0].playbackRate = speed;
+        currentSpeed = speed;
+        console.log("Setting new speed: " + speed);
+    }
 }
 
 function updateVideo() {
-    var curPos = position(allElements);
-    var elem = allElements[curPos];
+    var elem = getCurrentElem(allElements);
 
     if (elem.mapvideo != null 
-            && elem.mapvideo != currentVideo) {
-        currentBegin = elem.mapvideo_begin;
-        currentEnd = elem.mapvideo_end;
+            && elem.mapvideo != currentElem.mapvideo) {
         playNewVideo(elem);
     }
-    var newSpeed = getSpeed();
-    if (newSpeed != currentSpeed) {
-        setSpeed(newSpeed);
+
+    if (typeof elem.mapvideo_begin != 'undefined') {
+        var newSpeed = getVideoSpeed(elem, elem.mapvideo_begin, elem.mapvideo_end, currentVideoLength, timerMap);
+        if (newSpeed != currentSpeed) {
+            setSpeed(newSpeed);
+        }
     }
 
     if (elem != currentElem) {
-        // TODO
-            animate_lamp_icon(elem.light);
-
-          display_status(elem.name);
-            currentElem  = elem;
+        animate_lamp_icon(elem.light);
+        display_status(elem.name);
+        currentElem  = elem;
     }
 }
 
